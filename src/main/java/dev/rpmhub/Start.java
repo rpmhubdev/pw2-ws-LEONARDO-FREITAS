6b40571323cd0e1dd871061cc3e9f8fe774dbf78
LEO-SF <71852506+LEO-SF@users.noreply.github.com>
2023-08-20 22:10:22 +0000

    Implementados conversões

--- src/main/java/dev/rpmhub/Start.java
+++ src/main/java/dev/rpmhub/Start.java
@@ -1,18 +1,34 @@
 package dev.rpmhub;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/api")
 public class Start {
 
-    @GET
-    @Path("/hello")
+    @POST
+    @Path("/miles")
+    @Consumes("application/x-www-form-urlencoded; charset = UTF-8")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy";
+    public String miles(@FormParam("milhas") double milhas) {
+        double multiplicador = 0.62137;
+        double conversor = (milhas*multiplicador);
+        return String.valueOf(conversor);
+    }
+
+    @GET
+    @Path("/kilo/{quilometros}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String kilo(@PathParam("quilometros") double quilometros) {
+        double divisor = 1.852;
+        double conversor = (quilometros*divisor);
+        return String.valueOf(conversor);
     }
 
 }
